854b2792be5eae0e018e91c52062b8b0627e1d29
George Campbell <campbelg@oclc.org>
2018-08-28 15:14:41 -0400

    Start the notes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 const R = require('ramda');
 
-console.log(R);
+let a = R.add(2,3)
+console.log(a)  //  5
+let b = R.add(5)(5)
+console.log(b)  // 10
+
+// Curry R.add()
+let c = R.add(9)
+console.log(c)  // [Function: f1]
+let d = c(10)
+console.log(d)  // 15
